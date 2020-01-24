b28c73c96f90d314bf64557e0c4ade4f75f2cd32
EliseoVM <elyzeo.vm@gmail.com>
2020-01-24 15:45:36 -0600

    Capturando los datos del formulario

--- pagar.php
+++ pagar.php
@@ -4,14 +4,20 @@ if (!isset($_POST['producto'], $_POST['precio'])) {
     exit("Hubo un error");
 }
 
+use PayPal\Api\Payer;
+
 require 'config.php';
 
-// Sanitizacion
 $produto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
 $precio = (int) $precio;
 var_dump($precio);
 $envio = 0;
 $total = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+echo $compra->getPaymentMethod();
 
 ?>
